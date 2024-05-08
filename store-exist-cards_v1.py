7b717f0706fbcef4b5fe8500dd5ddd0d6406e1fe
mgsthomasxu <xut2@middleton.school.nz>
2024-05-08 14:46:11 +1200

    :+1:

---
+++ store-exist-cards_v1.py
@@ -0,0 +1,14 @@
+###component 1 store exist cards###
+
+exist_cards = {
+    "Stoneling":{
+        "Strength": 7,
+        "Speed": 1,
+        "Stealth": 25,
+        "Cunning": 15,
+    }
+    "Vexscream": {
+        "Strength": 1,
+    }
+}
+print("nigga")
